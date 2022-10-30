7ffb5f2eab2bf2c15bedfb4722e25673f3c80964
GeorgGrech <56017225+GeorgGrech@users.noreply.github.com>
2022-10-30 18:40:17 +0100

    Unity Navmesh and Player-Resource distance
    -Created versions of agent  prefab and script using unity navmesh instead of A*
    -Calculated distance between enemy and all resources

--- Assets/ThesisProject/Scripts/EnemyAgentNavmesh.cs
+++ Assets/ThesisProject/Scripts/EnemyAgentNavmesh.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
-using Pathfinding;
+//using Pathfinding;
+using UnityEngine.AI;
 
 /// <summary>
 /// EnemyAgent class, inheritng base ML Agent class, to handle all decisions and direct actions, including ones from ParentPlayer class
 /// that may be grouped together as one action (i.e "Go to base" that includes setting target to base and interacting)
 /// </summary>
-/*
+
 public class ResourceData
 {
     public GameObject resourceObject;
@@ -20,7 +21,7 @@ public class ResourceData
 }
 
 
-public class EnemyAgent : Agent
+public class EnemyAgentNavmesh : Agent
 {
     private EnemyPlayer enemyPlayer;
     private Transform enemyBase;
@@ -29,28 +30,20 @@ public class EnemyAgent : Agent
     private GameManager gameManager;
 
     public List<ResourceData> resourcesTrackingList;
-
-
-
-    private Seeker seeker;
-    private AIPath aiPath;
-    private AIDestinationSetter destinationSetter;
+    private NavMeshAgent navmeshAgent;
     //private List<GameObject> resourceObjects;
 
     //public Transform target;
     // Start is called before the first frame update
     void Start()
     {
-        destinationSetter = GetComponent<AIDestinationSetter>();
-        aiPath = GetComponent<AIPath>();
-        seeker = GetComponent<Seeker>();
-
         //playerScript = gameObject.GetComponent<ParentPlayer>();
         enemyPlayer = gameObject.AddComponent<EnemyPlayer>();
         enemyBase = GameObject.FindGameObjectWithTag("EnemyBase").transform;
 
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 
+        navmeshAgent = GetComponent<NavMeshAgent>();
         //PopulateTrackingList();
     }
 
@@ -58,38 +51,33 @@ public class EnemyAgent : Agent
     void Update()
     {
         //A* Test method. Be sure to remove.
-        //AStarTest();
+        AStarTest();
     }
 
+
     [ContextMenu("Update Distances")]
-    public void GetTrackingList()
+    public void TrackingListActivate()
     {
-        StartCoroutine(TrackingListCoroutine());
+        StartCoroutine(GetTrackingList());
     }
-    
-    public IEnumerator TrackingListCoroutine()
+
+    public IEnumerator GetTrackingList()
     {
         gameManager.ClearNullValues(); //Clear null values from gameManager.ResourceOhjects
+
 
         resourcesTrackingList = new List<ResourceData>();
         foreach (GameObject resourceObject in gameManager.ResourceObjects)
         {
-            //1. Get distance player to resource
-            aiPath.destination = resourceObject.transform.position;
-            int counter = 0;
-            while (aiPath.pathPending) //Wait until path is processed
+            navmeshAgent.destination = resourceObject.transform.position;
+
+            while(GetPathRemainingDistance() == -1)
             {
-                Debug.Log("Counter: " + counter + " aiPath.pathPending: " + aiPath.pathPending);
-                counter++;
                 yield return null;
             }
-            float distanceFromPlayer = aiPath.remainingDistance;
-            Debug.Log("Counter: " + counter + " aiPath.pathPending: " + aiPath.pathPending);
 
-            //2. Get distance resource to base
+            float distanceFromPlayer = GetPathRemainingDistance();
 
-
-            Debug.Log("Resource position: " + resourceObject.transform.position);
             resourcesTrackingList.Add(new ResourceData()
             {
                 resourceObject = resourceObject,
@@ -102,15 +90,15 @@ public class EnemyAgent : Agent
         }
 
         //Redundant loop just for checking due to inability to see resourcesTrackingList in inspector
-        foreach(ResourceData resourceData in resourcesTrackingList)
+        foreach (ResourceData resourceData in resourcesTrackingList)
         {
             Debug.Log("ResourceData added- Type: " + resourceData.type +
                 " DistFromPlayer: " + resourceData.distanceFromPlayer +
                 " DistFromBase: " + resourceData.distanceFromBase);
         }
     }
-    
-    
+
+    /*
     public void UpdateTrackingDistances()
     {
         foreach (ResourceData resourceData in resourcesTrackingList)
@@ -127,6 +115,22 @@ public class EnemyAgent : Agent
             resourceData.distanceFromPlayer = enemyPlayer.CalculateAStarDistance(resourcePosition);
             resourceData.distanceFromBase = resourceData.resourceObject.GetComponent<ResourceObject>().CalculateAStarDistance(enemyBase.position);
         }
+    }*/
+
+    public float GetPathRemainingDistance()
+    {
+        if (navmeshAgent.pathPending ||
+            navmeshAgent.pathStatus == NavMeshPathStatus.PathInvalid ||
+            navmeshAgent.path.corners.Length == 0)
+            return -1f;
+
+        float distance = 0.0f;
+        for (int i = 0; i < navmeshAgent.path.corners.Length - 1; ++i)
+        {
+            distance += Vector3.Distance(navmeshAgent.path.corners[i], navmeshAgent.path.corners[i + 1]);
+        }
+
+        return distance;
     }
 
     public void GatherResource()
@@ -155,14 +159,14 @@ public class EnemyAgent : Agent
         // 4. Wait until completion
     }
 
- 
+    /*
     public Transform DestinationFinder()
     {
         //Find destination
         return null;
     }*/
 
-   /*public override void CollectObservations(VectorSensor sensor)
+    public override void CollectObservations(VectorSensor sensor)
     {
         /* Observations to collect
          * -(All resources) Distance from enemy
@@ -175,45 +179,43 @@ public class EnemyAgent : Agent
         sensor.AddObservation(enemyBase.transform.localPosition); // Position of enemy base
         sensor.AddObservation(this.transform.localPosition); // Position of enemy
         */
-    //}
+    }
 
 
     /// <summary>
     /// Test variable and method to check A* functionality. Remove or comment later.
     /// </summary>
-    /*
- Transform player;
+
+    Transform player;
     void AStarTest()
     {
-        if(!player)
+        if (!player)
             player = GameObject.Find("Player").transform;
         else
             StartCoroutine(enemyPlayer.GoToDestination(player));
     }
-}*/
+}
 
 /// <summary>
 /// EnemyPlayer class, inheriting ParentPlayer script, to handle basic actions such as Interaction as well as inventory management.
 /// Set as separate class due to inability to inherit both Agent and ParentPlayer. Not set as ParentPlayer component for easier management.
 /// </summary>
-/*
+
 public class EnemyPlayer : ParentPlayer
 {
+    /*
     //A* variables
     private Seeker seeker;
     private AIPath aiPath;
     private AIDestinationSetter destinationSetter;
+    */
 
+    private NavMeshAgent navmeshAgent;
     protected override void Start()
     {
         base.Start();
-        
-        destinationSetter = GetComponent<AIDestinationSetter>();
-        aiPath = GetComponent<AIPath>();
-        seeker = GetComponent<Seeker>();
-        
-        //Set correctly later so aiPath.maxSpeed updates with base.movementSpeed
-        aiPath.maxSpeed = base.movementSpeed;
+        navmeshAgent = GetComponent<NavMeshAgent>();
+
 
     }
 
@@ -224,6 +226,7 @@ public class EnemyPlayer : ParentPlayer
 
     public IEnumerator GoToDestination(Transform destination)
     {
+        /*
         //destinationSetter.target = destination;
         aiPath.destination = destination.position;
         //ai.SearchPath();
@@ -233,17 +236,17 @@ public class EnemyPlayer : ParentPlayer
         }
 
         Interact();
-
-        //Yield wait 
+        */
+        yield return null;
     }
 
-    public float CalculateAStarDistance(Vector3 objectToCheck)
+
+    /*public float CalculatePathDistance(Vector3 objectToCheck)
     {
         //1. Briefly set object to check as A* destination
-        aiPath.destination = objectToCheck;
+        navmeshAgent.destination = objectToCheck;
         //2. Return distance as float
-        return aiPath.remainingDistance;
-    }
+        return navmeshAgent.remainingDistance;
+    }*/
 
-}*/
-
+}
